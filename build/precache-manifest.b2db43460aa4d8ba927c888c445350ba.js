2d35464b3362baafe6eab511873ac9b4914f6232
Aristides Mairena <aristides.mairena@usask.ca>
2019-03-20 15:45:53 -0600

    aded react pokedex

---
+++ build/precache-manifest.b2db43460aa4d8ba927c888c445350ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a398990b1d7b6c11135bd5823555f99",
+    "url": "/static/media/sprites.6a398990.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3e3541b9131d16c866d7",
+    "url": "/static/js/main.7aed840e.chunk.js"
+  },
+  {
+    "revision": "6b9f42d2d341c69195cf",
+    "url": "/static/js/2.236f05ae.chunk.js"
+  },
+  {
+    "revision": "3e3541b9131d16c866d7",
+    "url": "/static/css/main.0cfed2b2.chunk.css"
+  },
+  {
+    "revision": "fbd7728069b2ad449198bee5c6128b4a",
+    "url": "/index.html"
+  }
+];
